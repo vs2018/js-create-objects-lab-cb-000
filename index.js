8be3619b414aa198183e4ed7cc5e4e854660d390
Vishal Sharma <vsh1819@gmail.com>
2018-08-10 03:38:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ funtion Dog (name, breed, age) {
   this.age = age
 }
 
+funtion Cat (name, breed, age) {
+  this.name = name
+  this.breed = breed
+  this.age = age
+}
+
 funtion Cog (name, breed, age) {
   this.name = name
   this.breed = breed
   this.age = age
 }
+
